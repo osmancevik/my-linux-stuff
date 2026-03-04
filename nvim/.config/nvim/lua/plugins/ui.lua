c93dd7e70e44d707fcbf6286e87029d66668d110
osmancevik <github.fencing841@passmail.net>
2026-03-04 16:20:06 +0300

    ui.lua

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -11,7 +11,7 @@ return {
     end,
   },
 
-  -- 2. Treesitter (Dil desteği)
+  -- 2. Treesitter (Dil desteği ve Performans ayarı)
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
@@ -19,7 +19,32 @@ return {
         opts.ensure_installed = { "lua", "vim", "vimdoc", "query" }
       end
       vim.list_extend(opts.ensure_installed, { "latex", "markdown", "markdown_inline", "css" })
+
+      -- Büyük dosyalarda performansı korumak için highlight kısıtlaması
+      opts.highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      }
       return opts
     end,
+  },
+
+  -- 3. Neo-tree (Dosya gezgini senkronizasyonu)
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        -- JetBrains benzeri: Açık olan dosyayı ağaç yapısında otomatik bulur
+        follow_current_file = { enabled = true },
+        -- Dosya sistemindeki değişiklikleri (yeni dosya vb.) anlık izler
+        use_libuv_file_watcher = true,
+      },
+    },
   },
 }
