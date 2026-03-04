72b9a72fd139fb78f9b685a8897e230ddd85686e
osmancevik <github.fencing841@passmail.net>
2026-03-04 16:26:25 +0300

    ui.lua

--- nvim/.config/nvim/lua/plugins/autosave.lua
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -3,17 +3,33 @@ return {
     "Pocco81/auto-save.nvim",
     opts = {
       enabled = true,
-      -- JetBrains gibi pencere değiştiğinde veya yazma durduğunda tetikle
-      trigger_events = { "InsertLeave", "TextChanged", "FocusLost" }, 
+      trigger_events = { "InsertLeave", "TextChanged" },
       condition = function(buf)
         local fn = vim.fn
+        local api = vim.api
+
+        -- 1. Tamponun hala geçerli olup olmadığını kontrol et (Hatanın çözümü)
+        if not buf or not api.nvim_buf_is_valid(buf) then
+          return false
+        end
+
+        -- 2. Sadece normal dosyalarda çalış (Mevcut kontrolün)
         local buftype = fn.getbufvar(buf, "&buftype")
-        -- Sadece normal dosyalarda ve yazılabilir alanlarda çalış
-        if buftype ~= "" then return false end
+        if buftype ~= "" then
+          return false
+        end
+
         return true
       end,
       write_all_buffers = false,
-      debounce_delay = 200, -- Çok sık kaydedip diski yormaması için kısa bir gecikme
+      debounce_delay = 135,
+      callbacks = {
+        after_saving = function()
+          if Snacks then
+            Snacks.notify.info("Değişiklikler kaydedildi", { title = "AutoSave", timeout = 1000 })
+          end
+        end,
+      },
     },
   },
 }
