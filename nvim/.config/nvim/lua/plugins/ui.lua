5cc2013ef1c7dc90aff6c9e71b1a42942778a24b
osmancevik <github.fencing841@passmail.net>
2026-04-02 20:21:49 +0300

    -

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -17,9 +17,9 @@ return {
     opts = function(_, opts)
       opts.highlight = {
         enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024
+          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true
           end
