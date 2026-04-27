88a3e64ad35b5623a996c05f73bac539545dadd1
osmancevik <github.fencing841@passmail.net>
2026-04-27 10:52:36 +0300

    -

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,8 +9,9 @@ opt.scrolloff = 10
 opt.signcolumn = "yes"
 
 -- Indentation settings
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
 opt.expandtab = true
 opt.smartindent = true
 
