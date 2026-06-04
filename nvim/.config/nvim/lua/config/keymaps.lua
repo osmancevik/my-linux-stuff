b6f3aaa6878dcc9af6cc9a892e1876e05eef5ddc
osmancevik <github.fencing841@passmail.net>
2026-06-04 17:55:32 +0300

    .

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- JetBrains Style Run (Python, C, etc.)
-vim.keymap.set("n", "<F5>", function()
-  require("utils.runner").run_file()
-end, { desc = "JetBrains Style Run (Wait for Enter)" })
