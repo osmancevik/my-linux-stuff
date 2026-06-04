af12e47bd24672a669c52e7aa5b48253ad6b0b6c
osmancevik <github.fencing841@passmail.net>
2026-06-04 14:53:50 +0300

    .

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,11 +1,21 @@
+-- completion.lua: configuration for blink.cmp auto completion
+
 return {
   {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        preset = "enter", -- Enter ile onaylama varsayılan kalsın
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+        ["<Up>"] = { "fallback" },
+        ["<Down>"] = { "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+      },
+      -- prevent auto focus to completion menu
+      completion = {
+        list = {
+          selection = { preselect = false, auto_insert = false },
+        },
       },
     },
   },
